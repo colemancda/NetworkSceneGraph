7a0b7f28c37afa16803116db847d64d299e89a56
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-05-26 22:09:45 -0400

    first commit

---
+++ NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.h
@@ -0,0 +1,13 @@
+//
+//  NetworkSceneGraph.h
+//  NetworkSceneGraph
+//
+//  Created by Alsey Coleman Miller on 5/23/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkSceneGraph : NSObject
+
+@end
---
+++ NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.m
@@ -0,0 +1,13 @@
+//
+//  NetworkSceneGraph.m
+//  NetworkSceneGraph
+//
+//  Created by Alsey Coleman Miller on 5/23/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import "NetworkSceneGraph.h"
+
+@implementation NetworkSceneGraph
+
+@end
