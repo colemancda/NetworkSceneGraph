e36f702d9d13b81e5ab5351ca5a4bb1316ba50cf
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-05-28 08:52:52 -0400

    deleted NSGFloor
    There shouldn’t be specific classes for geometries. Floor geometries
    and other specific primitives will be creates manually or with a
    category.

---
+++ NetworkSceneGraph/NetworkSceneGraph/NSGGeometry+SCNGeometry.h
@@ -0,0 +1,13 @@
+//
+//  NSGGeometry+SCNGeometry.h
+//  NetworkSceneGraph
+//
+//  Created by Alsey Coleman Miller on 5/28/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import "NSGGeometry.h"
+
+@interface NSGGeometry (SCNGeometry)
+
+@end
