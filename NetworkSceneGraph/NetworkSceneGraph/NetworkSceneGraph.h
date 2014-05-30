20726a3a89c54deba086ae14ba504d7cb61b342c
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-05-30 09:10:42 -0400

    updated framework header

--- NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.h
+++ NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.h
@@ -9,8 +9,25 @@
 #import <NetworkObjects/NetworkObjects.h>
 
 #import <NetworkSceneGraph/NSGScene.h>
-#import <NetworkSceneGraph/NSNode.h>
-#import <NetworkSceneGraph/NSGeometry.h>
+#import <NetworkSceneGraph/NSGNode.h>
+#import <NetworkSceneGraph/NSGGeometry.h>
+#import <NetworkSceneGraph/NSGGeometrySource.h>
+#import <NetworkSceneGraph/NSGGeometryElement.h>
+#import <NetworkSceneGraph/NSGLink.h>
+#import <NetworkSceneGraph/NSGLight.h>
+#import <NetworkSceneGraph/NSGCamera.h>
+#import <NetworkSceneGraph/NSGMaterial.h>
+#import <NetworkSceneGraph/NSGMaterialProperty.h>
+#import <NetworkSceneGraph/NSGMaterialPropertyContent.h>
+#import <NetworkSceneGraph/NSGMaterialPropertyContentVideoHLS.h>
+#import <NetworkSceneGraph/NSGMaterialPropertyContentColor.h>
+#import <NetworkSceneGraph/NSGMaterialPropertyContentImage.h>
+#import <NetworkSceneGraph/NSGMaterialPropertyContentImageURL.h>
+#import <NetworkSceneGraph/NSGSound.h>
+#import <NetworkSceneGraph/NSGSoundDataSource.h>
+#import <NetworkSceneGraph/NSGSoundDataSourceData.h>
+#import <NetworkSceneGraph/NSGSoundDataSourceHLSStream.h>
+
 
 // Additions
 
