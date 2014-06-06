3d99455aba8560987461bba9468ba46f3666cbfc
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-06-06 11:58:04 -0400

    recreated project as Swift framework

--- NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.h
+++ NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.h
@@ -2,41 +2,18 @@
 //  NetworkSceneGraph.h
 //  NetworkSceneGraph
 //
-//  Created by Alsey Coleman Miller on 5/23/14.
+//  Created by Alsey Coleman Miller on 6/6/14.
 //  Copyright (c) 2014 ColemanCDA. All rights reserved.
 //
 
-#import <NetworkObjects/NetworkObjects.h>
+#import <Cocoa/Cocoa.h>
 
-#import <NetworkSceneGraph/NSGScene.h>
-#import <NetworkSceneGraph/NSGNode.h>
-#import <NetworkSceneGraph/NSGGeometry.h>
-#import <NetworkSceneGraph/NSGGeometrySource.h>
-#import <NetworkSceneGraph/NSGGeometryElement.h>
-#import <NetworkSceneGraph/NSGLink.h>
-#import <NetworkSceneGraph/NSGLight.h>
-#import <NetworkSceneGraph/NSGCamera.h>
-#import <NetworkSceneGraph/NSGMaterial.h>
-#import <NetworkSceneGraph/NSGMaterialProperty.h>
-#import <NetworkSceneGraph/NSGMaterialPropertyContent.h>
-#import <NetworkSceneGraph/NSGMaterialPropertyContentVideoHLS.h>
-#import <NetworkSceneGraph/NSGMaterialPropertyContentColor.h>
-#import <NetworkSceneGraph/NSGMaterialPropertyContentImage.h>
-#import <NetworkSceneGraph/NSGMaterialPropertyContentImageURL.h>
-#import <NetworkSceneGraph/NSGSound.h>
-#import <NetworkSceneGraph/NSGSoundDataSource.h>
-#import <NetworkSceneGraph/NSGSoundDataSourceData.h>
-#import <NetworkSceneGraph/NSGSoundDataSourceHLSStream.h>
+//! Project version number for NetworkSceneGraph.
+FOUNDATION_EXPORT double NetworkSceneGraphVersionNumber;
 
+//! Project version string for NetworkSceneGraph.
+FOUNDATION_EXPORT const unsigned char NetworkSceneGraphVersionString[];
 
-// Additions
-
-#import <NetworkSceneGraph/NSString+SCNVector3.h>
-#import <NetworkSceneGraph/NSString+SCNVector4.h>
-#import <NetworkSceneGraph/NSString+CGColorRef.h>
-#import <NetworkSceneGraph/NSManagedObjectModel+NetworkSceneGraph.h>
-#import <NetworkSceneGraph/NOServer+NetworkSceneGraph.h>
-#import <NetworkSceneGraph/NOAPICachedStore+NetworkSceneGraph.h>
-
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkSceneGraph/PublicHeader.h>
 
 
