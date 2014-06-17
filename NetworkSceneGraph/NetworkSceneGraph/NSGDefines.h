f0832cf58a0c7e9297f6ee48185d941d2e087ace
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-06-17 19:25:04 -0400

    changing vectors to multiple float properties instead of a single string property

---
+++ NetworkSceneGraph/NetworkSceneGraph/NSGDefines.h
@@ -0,0 +1,11 @@
+//
+//  NSGDefines.h
+//  NetworkSceneGraph
+//
+//  Created by Alsey Coleman Miller on 6/17/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
---
+++ NetworkSceneGraph/NetworkSceneGraph/NSGDefines.m
@@ -0,0 +1,10 @@
+//
+//  NSGDefines.m
+//  NetworkSceneGraph
+//
+//  Created by Alsey Coleman Miller on 6/17/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import "NSGDefines.h"
+
