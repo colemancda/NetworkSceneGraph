e14ba0018f19631082111fddddfecfb03da38545
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-10-05 13:39:18 -0400

    added NetworkSceneGraphManagedObjectModel constant
    also got the framework to properly compile for iOS / OS X

--- NetworkSceneGraph/NetworkSceneGraph/SceneController.swift
+++ NetworkSceneGraph/NetworkSceneGraph/SceneController.swift
@@ -10,7 +10,8 @@ import Foundation
 import SceneKit
 import CoreData
 
-let NetworkSceneGraphManagedObjectModel = NSBundle(identifier: "com.ColemanCDA.NetworkSceneGraph")
+/** The managed object model used by this NetworkSceneGraph. */
+let NetworkSceneGraphManagedObjectModel = NSManagedObjectModel(contentsOfURL: NSBundle(identifier: "com.ColemanCDA.NetworkSceneGraph")!.URLForResource("Model", withExtension: "momd")!)!
 
 /** This controller class will tell the self.viewScene to display the scene in self.representedScene. */
 @objc public class SceneController {
@@ -44,6 +45,13 @@ let NetworkSceneGraphManagedObjectModel = NSBundle(identifier: "com.ColemanCDA.N
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "managedObjectContextObjectsDidChange:", name: NSManagedObjectContextObjectsDidChangeNotification, object: self.managedObjectContext)
     }
     
+    private func initViewScene() -> SCNScene {
+        
+        let viewScene = SCNScene()
+        
+        return viewScene
+    }
+    
     // MARK: - Private Methods
     
     private func managedObjectContextObjectsDidChange(notification: NSNotification) {
@@ -51,13 +59,6 @@ let NetworkSceneGraphManagedObjectModel = NSBundle(identifier: "com.ColemanCDA.N
         let insertedObjects = notification.userInfo?[NSInsertedObjectsKey] as? [NSManagedObject]
         
         
-    }
-    
-    private func initViewScene() -> SCNScene {
-        
-        let viewScene = SCNScene()
-        
-        return viewScene
     }
     
     
