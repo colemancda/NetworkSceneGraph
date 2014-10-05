22cc02d5793e20f4c4a88ffe718455a090a98417
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-10-05 01:47:28 -0400

    created Swift framework project

---
+++ NetworkSceneGraph/NetworkSceneGraph/NetworkSceneGraph.h
@@ -0,0 +1,17 @@
+//
+//  NetworkSceneGraph.h
+//  NetworkSceneGraph
+//
+//  Created by Alsey Coleman Miller on 10/5/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for NetworkSceneGraph.
+FOUNDATION_EXPORT double NetworkSceneGraphVersionNumber;
+
+//! Project version string for NetworkSceneGraph.
+FOUNDATION_EXPORT const unsigned char NetworkSceneGraphVersionString[];
+
+
